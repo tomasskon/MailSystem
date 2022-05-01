d42a2a73563db900def5cc017a16afbaf5f9da37
Tomas Kondrotas <tomasskon@gmail.com>
2022-05-01 16:49:55 +0300

    JT-4 Adding server boiler plate

---
+++ MailSystem.Client/MailSystem.Contracts/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MailSystem.Contracts
+{
+    public class Class1
+    {
+    }
+}
---
+++ MailSystem.Server/MailSystem.Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MailSystem.Repositories
+{
+    public class Class1
+    {
+    }
+}
