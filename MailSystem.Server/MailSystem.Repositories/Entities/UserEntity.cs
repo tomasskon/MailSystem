4187883579022799e2d0cdabafa83f36474bf928
Tomas Kondrotas <tomasskon@gmail.com>
2022-05-25 00:33:04 +0300

    JT-4 Updating/Creating entities

--- MailSystem.Server/MailSystem.Repositories/Entities/UserEntity.cs
+++ MailSystem.Server/MailSystem.Repositories/Entities/UserEntity.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using FluentNHibernate.Data;
 using FluentNHibernate.Mapping;
-using NHibernate.Type;
 
 namespace MailSystem.Repositories.Entities
 {
     public class UserEntity : Entity
     {
         public virtual Guid Id { get; set; }
-        
-        public virtual string Username { get; set; }
 
         public virtual string FullName { get; set; }
 
@@ -26,7 +23,6 @@ namespace MailSystem.Repositories.Entities
         public UserEntityMap()
         {
             Id(x => x.Id).GeneratedBy.Guid();
-            Map(x => x.Username).Length(50).Not.Nullable();
             Map(x => x.FullName).Length(50).Not.Nullable();
             Map(x => x.Phone).Length(50).Not.Nullable();
             Map(x => x.Email).Length(50).Not.Nullable();
