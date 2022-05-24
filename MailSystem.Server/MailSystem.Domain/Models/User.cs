e2c63896967a4d444f0c6e79222e4b4a324bd246
Tomas Kondrotas <tomasskon@gmail.com>
2022-05-25 00:32:44 +0300

    JT-4 Updating/Creating models

--- MailSystem.Server/MailSystem.Domain/Models/User.cs
+++ MailSystem.Server/MailSystem.Domain/Models/User.cs
@@ -6,8 +6,6 @@ namespace MailSystem.Domain.Models
     {
         public Guid Id { get; set; }
 
-        public string Username { get; set; }
-
         public string FullName { get; set; }
         
         public string Phone { get; set; }
@@ -15,12 +13,5 @@ namespace MailSystem.Domain.Models
         public string Email { get; set; }
 
         public bool IsDisabled { get; set; }
-
-        public DateTime CreatedAt { get; set; }
-
-        public DateTime UpdatedAt { get; set; }
-
-        public DateTime DeletedAt { get; set; }
-
     }
 }
