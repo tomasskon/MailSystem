afec0f036259cf21f8fdbb2e64bfcf433b009d3c
Ignas Gilys <gilys.ignas@gmail.com>
2022-05-26 16:54:36 +0300

    JT-61 Added mailbox controller and contract.

---
+++ MailSystem.Server/MailSystem.Contracts/Mailboxes/MailboxContract.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MailSystem.Contracts.Mailboxes
+{
+    public class MailboxContract
+    {
+        public Guid Id { get; set; }
+
+        public string Location { get; set; }
+        
+    }
+}
