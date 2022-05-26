93dca6917db75354245912df1712d2766dca8635
Ignas Gilys <gilys.ignas@gmail.com>
2022-05-26 16:54:06 +0300

    JT-60 Added mailbox service.

---
+++ MailSystem.Server/MailSystem.Services/Services/MailboxService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using MailSystem.Domain.Models;
+using MailSystem.Repositories.Interfaces;
+using MailSystem.Services.Interfaces;
+
+namespace MailSystem.Services.Services
+{
+    public class MailboxService : IMailboxService
+    {
+        private readonly IMailboxRepository _mailboxRepository;
+
+        public MailboxService(IMailboxRepository mailboxRepository)
+        {
+            _mailboxRepository = mailboxRepository;
+        }
+
+        public IEnumerable<Mailbox> GetAll()
+        {
+            return _mailboxRepository.GetAll();
+        }
+    }
+}
